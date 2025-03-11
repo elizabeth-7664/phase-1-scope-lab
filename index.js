b9899ee21fd7b9951fa1abfa0108043f63a2ee74
elizabeth ndinda <elizabethndinda41@gmail.com>
2025-03-11 09:59:01 +0300

    how functions determine scope in javascript

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+var bestCustomer;
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "someone"; 
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "another person"; 
+}
